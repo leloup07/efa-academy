6acd1b1f20ba52e53bf03058e8cff49b337fd042
Enrique Aznar <tu_email@loquesea.com>
2026-01-02 15:55:37 +0100

    Deploy ping

--- app.py
+++ app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="EFA Academy", layout="wide")
 st.title("EFA Academy")
 st.success("Deploy limpio y funcional 🚀")
+# deploy ping Fri Jan  2 15:55:37 CET 2026
