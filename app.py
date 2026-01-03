f8f30fcf90eb5e2cc0ff751a5783b258a514cc23
Enrique Aznar <tu_email@loquesea.com>
2026-01-03 23:21:44 +0100

    Añadir 50 preguntas oficiales EFA + casos prácticos

--- app.py
+++
@@ -1,9 +0,0 @@
-from data.db.init_progreso import init_progreso
-init_progreso()
-
-import streamlit as st
-
-st.set_page_config(page_title="EFA Academy", layout="wide")
-
-st.title("📘 EFA Academy")
-st.success("Streamlit funcionando en Railway 🚀")
